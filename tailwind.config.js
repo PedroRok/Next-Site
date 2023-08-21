4d4dfc6444ec896306b9988844d48149edfeea82
Rok <rexblanegamer@gmail.com>
2023-08-21 02:53:36 -0300

    tailwind changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,7 @@
 /** @type {import('tailwindcss').Config} */
+
+
+
 module.exports = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
@@ -7,11 +10,58 @@ module.exports = {
   ],
   theme: {
     extend: {
+      dropShadow: {
+        'glow': '0 0 var(--glowing, 8px) var(--glowing-color, rgba(255, 255, 255, 0.3))',
+      },
+      fontFamily: {
+        'trip': ['Trip Sans', 'sans-serif'],
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        'noise': "url('/img/noise.png')",
       },
+      keyframes: {
+        'fade-in-down': {
+          '0%': {
+            opacity: '0',
+            transform: 'translateY(-10px)',
+          },
+          '100%': {
+            opacity: '1',
+            transform: 'translateY(0)',
+          },
+        },
+        'fade-out-left': {
+          '0%': {
+            opacity: '1',
+            transform: 'translateX(0)',
+            letterSpacing: '0.0em',
+          },
+          '100%': {
+            opacity: '0',
+            transform: 'translateX(-30px)',
+            letterSpacing: '-0.1em',
+          },
+        },
+        'fade-in-left': {
+          '0%': {
+            opacity: '0',
+            transform: 'translateX(-30px)',
+            letterSpacing: '0.10em',
+          },
+          '100%': {
+            opacity: '1',
+            transform: 'translateX(0)',
+            letterSpacing: '0em',
+          },
+        },
+      },
+      animation: {
+        'fade-in-down': 'fade-in-down 0.5s ease-out',
+        'fade-out-left': 'fade-out-left 0.5s ease-out',
+        'fade-in-left': 'fade-in-left 0.5s ease-out',
+      }
     },
   },
   plugins: [],
