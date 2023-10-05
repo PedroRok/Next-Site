dc358aded29bdc685d67c67614a83c06333e3ebb
rok <rexblanegamer@gmail.com>
2023-10-05 00:07:04 -0300

    Formating icons

--- src/assets/twitterIcon.tsx
+++ src/assets/twitterIcon.tsx
@@ -4,9 +4,9 @@ export default function twitterIcon() {
 			<path
 				fill="none"
 				stroke="currentColor"
-				stroke-linecap="round"
-				stroke-linejoin="round"
-				stroke-width="2"
+				strokeLinecap="round"
+				strokeLinejoin="round"
+				strokeWidth="2"
 				d="m4 4l11.733 16H20L8.267 4zm0 16l6.768-6.768m2.46-2.46L20 4"
 			/>
 		</svg>
--- src/assets/youtubeIcon.tsx
+++ src/assets/youtubeIcon.tsx
@@ -4,9 +4,9 @@ export default function youtubeIcon() {
 			<g
 				fill="none"
 				stroke="currentColor"
-				stroke-linecap="round"
-				stroke-linejoin="round"
-				stroke-width="2"
+				strokeLinecap="round"
+				strokeLinejoin="round"
+				strokeWidth="2"
 			>
 				<path d="M2 8a4 4 0 0 1 4-4h12a4 4 0 0 1 4 4v8a4 4 0 0 1-4 4H6a4 4 0 0 1-4-4V8z" />
 				<path d="m10 9l5 3l-5 3z" />
