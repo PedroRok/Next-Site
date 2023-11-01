ee6781f153571c78def7f30ca9257c5e6c9ade83
rok <rexblanegamer@gmail.com>
2023-11-01 14:23:33 -0300

    Twitch icon implemented

--- src/components/SocialMedia.tsx
+++ src/components/SocialMedia.tsx
@@ -4,21 +4,23 @@ import SiteIcon from "@/assets/siteIcon";
 import TwitterIcon from "@/assets/twitterIcon";
 import YoutubeIcon from "@/assets/youtubeIcon";
 import DiscordIcon from "@/assets/discordIcon";
+import TwitchIcon from "@/assets/twitchIcon";
 
-export function getSocialMedia(key: string, animated?:boolean) {
-    switch (key) {
-        case "twitter":
-            return <TwitterIcon/>
-        case "instagram":
-            return <InstagramIcon/>
-        case "youtube":
-            return <YoutubeIcon/>
-        case "site":
-            return <SiteIcon/>
-        case "github":
-            return <GithubIcon strokeWidth={2.2} animated={animated}/>
-        case "discord":
-            return <DiscordIcon animated={animated}/>
-            
-    }
+export function getSocialMedia(key: string, animated?: boolean) {
+	switch (key) {
+		case "twitter":
+			return <TwitterIcon />;
+		case "instagram":
+			return <InstagramIcon />;
+		case "youtube":
+			return <YoutubeIcon />;
+		case "site":
+			return <SiteIcon />;
+		case "github":
+			return <GithubIcon strokeWidth={2.2} animated={animated} />;
+		case "discord":
+			return <DiscordIcon animated={animated} />;
+		case "twitch":
+			return <TwitchIcon />;
+	}
 }
