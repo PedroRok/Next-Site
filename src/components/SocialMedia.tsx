704240e5be8cebdcf8a8b2ed0f7b7185b7606471
rok <rexblanegamer@gmail.com>
2023-11-09 15:10:01 -0300

    Updating Reviews

--- src/components/SocialMedia.tsx
+++ src/components/SocialMedia.tsx
@@ -1,10 +1,12 @@
 import GithubIcon from "@/assets/githubIcon";
-import InstagramIcon from "@/assets/instagramIcon";
+import InstagramIcon from "@/assets/social/instagramIcon";
 import SiteIcon from "@/assets/siteIcon";
-import TwitterIcon from "@/assets/twitterIcon";
-import YoutubeIcon from "@/assets/youtubeIcon";
-import DiscordIcon from "@/assets/discordIcon";
-import TwitchIcon from "@/assets/twitchIcon";
+import TwitterIcon from "@/assets/social/twitterIcon";
+import YoutubeIcon from "@/assets/social/youtubeIcon";
+import DiscordIcon from "@/assets/social/discordIcon";
+import TwitchIcon from "@/assets/social/twitchIcon";
+import FacebookIcon from "@/assets/social/facebookIcon";
+import TiktokIcon from "@/assets/social/tiktokIcon";
 
 export function getSocialMedia(key: string, animated?: boolean) {
 	switch (key) {
@@ -22,5 +24,9 @@ export function getSocialMedia(key: string, animated?: boolean) {
 			return <DiscordIcon animated={animated} />;
 		case "twitch":
 			return <TwitchIcon />;
+		case "facebook":
+			return <FacebookIcon />;
+		case "tiktok":
+			return <TiktokIcon />;
 	}
 }
